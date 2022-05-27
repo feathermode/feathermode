1d1443753c48e57287280fa6f4f7be838bf81775
OOF <oof@motualis.com>
2022-05-27 10:54:57 -0400

    instructions

---
+++ functions/onthefly.js
@@ -0,0 +1,20 @@
+/*
+Steps
+1. Create form functionality and form sections
+2. Complete Discover Section
+3. Complete Design Section Shell
+4. Complete Develop Section Functions Select
+5. Complete Deploy Section Upsell Selects
+6. Complete Dedicate Plan Selection
+7. Create Test Site on Netlify and check Form Submission
+- 
+8. Finish Vibrant Plugin and test popup in form
+9. Finish Expressive Plugin and test popup in form
+10. Create global nav options
+11. Create global footwr options
+12. Create global button options
+13. Create global card options
+14. Create template page options with selections for blog, business, etc
+-
+15. Add Design Plugins to autofill form inputs
+*/
