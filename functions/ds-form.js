32f8c074279342bd8a3f76aad6985cf4f75a5839
oofytronic <oof@motualis.com>
2022-07-05 15:51:50 -0400

    reset totalStyles after each click

--- functions/ds-form.js
+++ functions/ds-form.js
@@ -10,6 +10,8 @@ squareGroups.forEach(group => {
 
 	squares.forEach(square => {
 		square.addEventListener('click', function(e) {
+			totalStyle = [];
+
 			squares.forEach(square => {
 				if (square.classList.contains("form_square-active")) {
 					square.classList.remove("form_square-active");
@@ -23,15 +25,17 @@ squareGroups.forEach(group => {
 				const select = group.querySelector("select");
 				const options = [...select.options];
 				select.value = selection;
-
-				const squareStyle = e.target.dataset.style;
-				totalStyle.push(squareStyle);
 			}
 
-			const select = group.querySelector("select");
-			console.log(select.value)
-			console.log(totalStyle)
+			const activeSquares = document.querySelectorAll(".form_square-active");
+			activeSquares.forEach(active => {
+				const squareStyle = active.dataset.style;
+				totalStyle.push(squareStyle);
+			});
 
+			console.log(totalStyle);
+
+			// APPLY COMBINED TEXT TO DEMO AREA WITH STYLE TAG
 			const demoArea = document.querySelector(".demo");
 			const demoAreaStyleTag = demoArea.querySelector("style");
 			if(typeof(demoAreaStyleTag) != 'undefined' && demoAreaStyleTag != null){
