778cd9eb7a81d48054903f6f49d0ce6aa37ba8a4
oofytronic <oof@motualis.com>
2023-04-19 18:17:21 -0400

    add banner

--- functions/hydrogen/README.md
+++
@@ -1,3 +0,0 @@
-# Hydrogen Templating Toolset
-Hydrogen is a micro templating engine used to extend javascript template literals and aid with visualization and fulfillment. 
-Hydrogen is also used to extend the abilities of HTML5 created content and CommonMark for user generated content.
--- functions/hydrogen/replace.js
+++
@@ -1,9 +0,0 @@
-/* */
-export function replace(element, replacement) {
-	let range = document.createRange();
-	let target = document.querySelector(element);
-	// let parentNode = document.querySelector(location);
-	const newElement = range.createContextualFragment(replacement);
-
-	target.replaceWith(newElement);
-}
