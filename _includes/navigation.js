53a7eca76a5f920b01bbe55f7b5e7233c9e4b6e7
OOF <oof@motualis.com>
2023-06-17 15:45:13 -0400

    main structures

--- _includes/navigation.js
+++ _includes/navigation.js
@@ -2,7 +2,13 @@ export default function navigation() {
 	return `
 		<nav class="flex items-center content-between padding-md">
 			<img class="nav-logo" src="/assets/fm_logo-white.svg" alt="feathermode">
-			<button>Free Web Consultation</button>
+			<ul class="flex gap-1 list-none">
+				<li><a href="#services">Services</a></li>
+				<li><a href="#process">Process</a></li>
+				<li><a href="#faq">FAQ</a></li>
+				<li><a href="">Blog</a></li>
+			</ul>
+			<a href="#sign-up">Free Web Consultation</a>
 		</nav>
 	`;
 }
--- home.js
+++ home.js
@@ -1,19 +1,5 @@
 export default function home(obj) {
 	return `
-		<!--
-		<section class="banner">
-			<img class="nav-logo" src="assets/fm_logo-white.svg" alt="feathermode"/>
-			<div class="card">
-				<p class="card-header">Something<br>WINGED<br>this<br>Way<br>Comes</p>
-				<p class="card-des">A New Web Platform Takes Flight | June 2023</p>
-			</div>
-			<footer>
-				<a class="" href="https://motualis.com" target="_blank" rel="noopener noreferrer"><img class="footer-icon" src="assets/motualis-icon_white.svg" alt="motuALIS"/></a>
-				<p>Copyright 2023 | motuALIS, LLC | All Rights Reserved</p>
-			</footer>
-		</section>
-		-->
-
 		<!-- HERO -->
 		<section>
 			<div class="wrapper">
@@ -21,7 +7,7 @@ export default function home(obj) {
 					<div class="card | flex flex-column gap-1">
 						<h1>Web Platforms <br/>Made to Fly</h1>
 						<p>We create custom websites and web applications, small and large, from the ground up to fit each unique story.</p>
-						<button>Learn About Our Services</button>
+						<a href="#services">Learn About Our Services</a>
 					</div>
 					<iframe src="https://app.vectary.com/p/7NxUVmbcUImjLSapgk009v" frameborder="0" width="100%" height="480"></iframe>
 				</div>
@@ -29,11 +15,11 @@ export default function home(obj) {
 		</section>
 
 		<!-- SERVICES -3  -->
-		<section>
+		<section id="services">
 			<div class="wrapper">
 				<div class="card | flex flex-column width-100">
 					<h2>Our Services</h2>
-					<div class="flex width-100">
+					<div class="flex gap-1 width-100">
 						<div>
 							<h3>Websites</h3>
 							<p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Nisi eaque ratione id! Distinctio tenetur atque perspiciatis consequuntur cumque eos odio quam. Molestias quis non quas odio, beatae. Esse, corporis, quibusdam.</p>
@@ -52,7 +38,7 @@ export default function home(obj) {
 		</section>
 
 		<!-- RECENT WORKS -->
-		<section>
+		<section id="">
 			<div class="wrapper">
 				<div class="card | flex flex-column">
 					<h2>Case Study</h2>
@@ -69,7 +55,7 @@ export default function home(obj) {
 		</section>
 
 		<!-- PROCESS -->
-		<section>
+		<section id="process">
 			<div class="wrapper">
 				<div>
 					<h2>Our Process</h2>
@@ -118,9 +104,52 @@ export default function home(obj) {
 		<!-- FROM WWWINGED -->
 
 		<!-- FAQ -->
-		
+		<section id="faq">
+			<div class="wrapper">
+				<div class="card width-100">
+					<h2>FAQ</h2>
+				</div>
+			</div>
+		</section>
 
 		<!-- FORM -->
-
+		<section id="sign-up">
+			<div class="wrapper">
+				<div class="card width-100">
+					<h2> Start Your Next Digital Story with a Free Web Consultation</h2>
+					<form class="flex flex-column gap-1" action="">
+						<label for="name">
+							Name	
+							<input id="name" name="name" type="text" required>
+						</label>
+						<label for="name">
+							Company (optional)
+							<input id="company" name="company" type="text">
+						</label>
+						<label for="email">
+							Email	
+							<input id="email" name="email" type="email" required>
+						</label>
+						<label for="name">
+							What type of Platform or Service are you interested in?	
+							<select class="width-fit-content" name="service" id="service" required>
+								<option value="websites">Website</option>
+								<option value="web-app">Web App</option>
+								<option value="content-marketing">Content Marketing</option>
+							</select>
+						</label>
+						<label for="on-the-web">
+							Do you have a live website or app? If so, let us know it's domain address.
+							<input name="on-the-web" type="text" required>
+						</label>
+						<label for="project-details">
+							Tell us more about your future Web Platform
+							<textarea name="project-details" id="project-details" cols="30" rows="10"></textarea required>
+						</label>
+						<button class="width-fit-content">Let it Fly!</button>
+					</form>
+				</div>
+			</div>
+		</section>
 	`;
 }
