5b039569def8c949e96125514775b47c12130ea6
OOF <oof@motualis.com>
2023-06-22 09:42:18 -0400

    select

--- _includes/footer.js
+++ _includes/footer.js
@@ -1,6 +1,6 @@
 export default function footer() {
 	return `
-		<footer>
+		<footer class="position-relative">
 			<a class="" href="https://motualis.com" target="_blank" rel="noopener noreferrer"><img class="footer-icon" src="assets/motualis-icon_white.svg" alt="motuALIS"></a>
 			<p>Copyright 2023 | motuALIS, LLC | All Rights Reserved</p>
 		</footer>
