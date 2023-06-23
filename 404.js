abd06294f4b037f01cc2968197d5ab3e63593faf
OOF <oof@motualis.com>
2023-06-23 19:38:30 -0400

    remove extra word

--- 404.js
+++ 404.js
@@ -4,7 +4,7 @@ export default function four(obj) {
 		<section class="position-relative overflow-hidden">
 			<div class="wrapper stack flex-center">
 				<h1 class="trigger opacity-0 fade-in">4oh4!</h1>
-				<p style="nax-width: 60ch;">You've found our 404 page which means you've requested a page that no longer exists or you've incorrectly queried for a page. Please check the domain address or navigate using the menu.</p>
+				<p style="max-width: 60ch;">You've found our 404 page which means you've requested a page that no longer exists or you've incorrectly queried for a page. Please check the domain address or navigate using the menu.</p>
 			</div>
 			<img class="hero_clouds" src="/assets/clouds.svg" alt="">
 		</section>
