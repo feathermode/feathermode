6055f7b3ea05e8b243dc18df852ded45f4b0d406
oofytronic <oof@motualis.com>
2023-06-30 19:43:33 -0400

    new thank you

--- thank_you.js
+++ thank_you.js
@@ -4,7 +4,9 @@ export default function thank_you() {
 		<section class="position-relative overflow-hidden">
 			<div class="wrapper stack flex-center">
 				<h1 class="trigger opacity-0 fade-in">Thank you!</h1>
-				<p style="max-width: 60ch;">I appreciate you reaching out to learn more about how feathermode and my 5D Web Creation Process can help your web platform fly! You'll receive an email from oof@feathermode.com with details on scheduling your web consultation with me.<br>- Oof</p>
+				<p style="max-width: 60ch;">I appreciate you reaching out to learn more about how feathermode and my 5D Web Creation Process can help your web platform fly! You'll receive an email from oof@feathermode.com with details on scheduling your web consultation with me.</p>
+				<br>
+				<p>- OOF</p>
 			</div>
 			<img class="hero_clouds" src="/assets/clouds.svg" alt="">
 		</section>
